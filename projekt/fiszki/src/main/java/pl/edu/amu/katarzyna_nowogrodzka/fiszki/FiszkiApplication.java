dfa41fe3775105d7090463a5f1c9a17cf7e5bef5
Katarzyna Nowogrodzka <kat.nowogrodzka@gmail.com>
2023-01-26 18:01:17 +0100

    project setup

---
+++ projekt/fiszki/src/main/java/pl/edu/amu/katarzyna_nowogrodzka/fiszki/FiszkiApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.amu.katarzyna_nowogrodzka.fiszki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiszkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiszkiApplication.class, args);
+	}
+
+}
---
+++ projekt/fiszki/src/test/java/pl/edu/amu/katarzyna_nowogrodzka/fiszki/FiszkiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.amu.katarzyna_nowogrodzka.fiszki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiszkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
