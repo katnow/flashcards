b6472d075856e46a76dea131e74280f91f53c88d
Katarzyna Nowogrodzka <kat.nowogrodzka@gmail.com>
2023-01-27 00:50:05 +0100

    adding flashcards test

--- projekt/fiszki/src/test/java/pl/edu/amu/katarzyna_nowogrodzka/fiszki/flashcard/FlashCardServiceTest.java
+++ projekt/fiszki/src/test/java/pl/edu/amu/katarzyna_nowogrodzka/fiszki/flashcard/FlashCardServiceTest.java
@@ -3,9 +3,11 @@ package pl.edu.amu.katarzyna_nowogrodzka.fiszki.flashcard;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 
 
@@ -25,6 +27,18 @@ public class FlashCardServiceTest {
     void canGetAllFlashCards() {
         underTest.getFlashCards();
         verify(flashCardRepository).findAll();
+    }
+
+    @Test
+    void canAddFlashCard() {
+        FlashCard flashCard = new FlashCard("kot", "cat");
+        underTest.addNewFlashCard(flashCard);
+
+        ArgumentCaptor<FlashCard> flashCardArgumentCaptor = ArgumentCaptor.forClass(FlashCard.class);
+        verify(flashCardRepository).save(flashCardArgumentCaptor.capture());
+
+        FlashCard capturedFlashCard = flashCardArgumentCaptor.getValue();
+        assertTrue(capturedFlashCard.equals(flashCard));
     }
 
 }
